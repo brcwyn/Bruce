e69070f3a666d7e02b3f0ba8f627bd98c2cd71fa
brcwyn <hariom3322266@gmail.com>
2025-03-31 21:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,19 +10,19 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org, --⚠️ REQUIRED--
-APP_ID = int(os.environ.get("APP_ID", "25195869"))
+APP_ID = int(os.environ.get("APP_ID", "27091024"))
 
 #Your API Hash from my.telegram.org, --⚠️ REQUIRED--
-API_HASH = os.environ.get("API_HASH", "711b4c998351c6969aff8f54a35327fa")
+API_HASH = os.environ.get("API_HASH", "3f64c85221e942491676b647ad98236a")
 
 #Your db channel Id --⚠️ REQUIRED--
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002694720727"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002444506966"))
 
 #OWNER ID --⚠️ REQUIRED--
-OWNER_ID = int(os.environ.get("OWNER_ID", "6093336723"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6098410989"))
 
 #SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "https://t.me/+-o9isg1zi01kOTk1")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "https://t.me/+frRMigCcumtmOWM9")
 
 #Port
 PORT = os.environ.get("PORT", "8080")
